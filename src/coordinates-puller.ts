1cc5c576f5cb620e3716f82a84c8779aeb19aebe
yonatanbeery <yonatanbeery@gmail.com>
2024-07-19 12:02:38 +0300

    added Point type

---
+++ src/coordinates-puller.ts
@@ -0,0 +1,14 @@
+type Point = {
+  lat: number;
+  long: number;
+  alt?: number;
+}
+
+const areaLimits: Point[] = [{
+  lat: 33.090000,
+  long: 35.080000
+},
+{
+  lat: 34.730000,
+  long: 36.660000
+}]
