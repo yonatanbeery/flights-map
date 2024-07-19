e9b04944f1e3e7d5cae1eab3c0afe6dd0b42b486
u9198081 <u9198081@bsmch.net>
2024-07-20 00:53:22 +0300

    rename consts in maxCoordinatesHeights.rs

--- src/maxCoordinatesHeight.ts
+++ src/maxCoordinatesHeight.ts
@@ -2,18 +2,18 @@ import {readCoordinatesFromFile} from './coordinateFormatter';
 import { Point } from './types';
 
 const pointDiff = 0.01;
-const latDiff = 1.112; //differance in km for 0.01 degrees
-const longDiff = 0.9317; //differance in km for 0.01 degrees
+const latCorrection = 1.112;
+const longCorrection = 0.9317;
 
 
 const isPointInRadius = (center: Point, point: Point, redius: number) => {
- return (((center.lat - point.lat) * latDiff / pointDiff) ** 2 + ((center.long - point.long) * longDiff / pointDiff) ** 2) < redius ** 2;
+ return (((center.lat - point.lat) * latCorrection / pointDiff) ** 2 + ((center.long - point.long) * longCorrection / pointDiff) ** 2) < redius ** 2;
 }
 
 const getMaxHeightInRadius = (heights: Point[][], point: Point, radius: number) => {
   let maxHeight: Point | undefined = undefined;
   
-  for (let lat = parseFloat((point.lat - radius * latDiff * pointDiff).toFixed(2)); lat <= point.lat + radius * latDiff * pointDiff ; lat = parseFloat((lat + pointDiff).toFixed(2))) {
+  for (let lat = parseFloat((point.lat - radius * latCorrection * pointDiff).toFixed(2)); lat <= point.lat + radius * latCorrection * pointDiff ; lat = parseFloat((lat + pointDiff).toFixed(2))) {
     //there isnt use in "longDiff" in porpuse. we are checking the worst case senario, and 0.9317 is not the worst case
     for (let long = parseFloat((point.long - radius * pointDiff).toFixed(2)); long <= point.long + radius * pointDiff; long = parseFloat((long + pointDiff).toFixed(2))) {           
       if (heights[lat] && heights[lat][long]) {
