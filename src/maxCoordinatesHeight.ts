df0ba522f414064aed39e79e6bc87e71d382700a
u9198081 <u9198081@bsmch.net>
2024-07-20 00:28:34 +0300

    add maxHeight.txt file

--- src/maxCoordinatesHeight.ts
+++ src/maxCoordinatesHeight.ts
@@ -1,19 +1,22 @@
 import {readCoordinatesFromFile} from './coordinateFormatter';
 import { Point } from './types';
 
-const latDiff = 1.112 / 0.01; //differance in km for 0.01 degrees
-const longDiff = 0.9317 / 0.01; //differance in km for 0.01 degrees
 const pointDiff = 0.01;
+const latDiff = 1.112; //differance in km for 0.01 degrees
+const longDiff = 0.9317; //differance in km for 0.01 degrees
+
 
 const isPointInRadius = (center: Point, point: Point, redius: number) => {
- return (((center.lat - point.lat) * latDiff) ** 2 + ((center.long - point.long) * longDiff) ** 2) < redius**2;
+ return (((center.lat - point.lat) * latDiff / pointDiff) ** 2 + ((center.long - point.long) * longDiff / pointDiff) ** 2) < redius ** 2;
 }
 
 const getMaxHeightInRadius = (heights: Point[][], point: Point, radius: number) => {
   let maxHeight: Point | undefined = undefined;
   
-  for (let lat = parseFloat((point.lat - radius * pointDiff).toFixed(2)); lat < point.lat + radius * pointDiff ; lat = parseFloat((lat + pointDiff).toFixed(2))) {
-    for (let long = parseFloat((point.long - radius * pointDiff).toFixed(2)); long < point.long + radius * pointDiff; long = parseFloat((long + pointDiff).toFixed(2))) {           if (heights[lat] && heights[lat][long]) {
+  for (let lat = parseFloat((point.lat - radius * latDiff * pointDiff).toFixed(2)); lat <= point.lat + radius * latDiff * pointDiff ; lat = parseFloat((lat + pointDiff).toFixed(2))) {
+    //there isnt use in "longDiff" in porpuse. we are checking the worst case senario, and 0.9317 is not the worst case
+    for (let long = parseFloat((point.long - radius * pointDiff).toFixed(2)); long <= point.long + radius * pointDiff; long = parseFloat((long + pointDiff).toFixed(2))) {           
+      if (heights[lat] && heights[lat][long]) {
         if (isPointInRadius(point, heights[lat][long], radius) && (!maxHeight || heights[lat][long].alt > maxHeight.alt)) {
           maxHeight = heights[lat][long];
         }
