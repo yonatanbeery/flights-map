f6131140c3b4d2935774b7fd7a04ce5eaf6d9908
yonatanbeery <yonatanbeery@gmail.com>
2024-07-20 12:56:04 +0300

    supposed to filter parallel points

--- src/logic/findGroups.ts
+++ src/logic/findGroups.ts
@@ -1,22 +1,38 @@
 import { Point } from "../utils/types";
-import { areaLimits, pointAccuracy } from "../utils/globals";
+import { pointAccuracy } from "../utils/globals";
 
-export const getBorderPoints = (heights: Point[][]):Point[] => {
+export const getBorderPoints = (polygon: Point[][]):Point[] => {
     const borderPoints: Point[] = [];
-    for(let lat = Number.parseFloat((areaLimits.min.lat).toFixed(2)); lat <= Number.parseFloat((areaLimits.max.lat).toFixed(2)); lat = Number.parseFloat((lat + 0.01).toFixed(2))) {
-        for(let long = Number.parseFloat((areaLimits.min.long).toFixed(2)); long <= Number.parseFloat((areaLimits.max.long).toFixed(2)); long = Number.parseFloat((long + 0.01).toFixed(2))) {
-            if(!soroundedBySameHeight(heights[lat][long], heights)) borderPoints.push(heights[lat][long])
-        }
-    }
+    Object.keys(polygon).forEach(lat => {
+        Object.keys(lat).forEach(long => {
+            if(!isNotBorderPoint(polygon[lat][long], polygon) && !isOnStrightBorderLine(polygon[lat][long], polygon)) borderPoints.push(polygon[lat][long])
+        })
+    })
     return borderPoints
 }
 
-const soroundedBySameHeight = (point: Point, heights: Point[][]):boolean => {
-    const isEdge = (point.lat == areaLimits.min.lat || point.long == areaLimits.min.long || 
-        point.lat == areaLimits.max.lat || point.long == areaLimits.max.long)        
-
-    return !isEdge && point.alt === heights[Number.parseFloat((point.lat + pointAccuracy).toFixed(2))][point.long].alt &&
-    point.alt === heights[point.lat][Number.parseFloat((point.long + pointAccuracy).toFixed(2))].alt &&
-    point.alt === heights[Number.parseFloat((point.lat - pointAccuracy).toFixed(2))][point.long].alt &&
-    point.alt === heights[point.lat][Number.parseFloat((point.long - pointAccuracy).toFixed(2))].alt
+const isNotBorderPoint = (point: Point, polygon: Point[][]):boolean => {
+    return !!polygon[Number.parseFloat((point.lat + pointAccuracy).toFixed(2))]?.[point.long] &&
+    !!polygon[point.lat]?.[Number.parseFloat((point.long + pointAccuracy).toFixed(2))] &&
+    !!polygon[Number.parseFloat((point.lat - pointAccuracy).toFixed(2))]?.[point.long] &&
+    !!polygon[point.lat]?.[Number.parseFloat((point.long - pointAccuracy).toFixed(2))]
 }
+
+const isOnStrightBorderLine = (point: Point, polygon: Point[][]):boolean => {
+    return (!!polygon[Number.parseFloat((point.lat + pointAccuracy).toFixed(2))]?.[point.long] &&
+    !!polygon[Number.parseFloat((point.lat - pointAccuracy).toFixed(2))]?.[point.long]) ||
+    (!!polygon[point.lat]?.[Number.parseFloat((point.long + pointAccuracy).toFixed(2))] &&
+    !!polygon[point.lat]?.[Number.parseFloat((point.long - pointAccuracy).toFixed(2))])
+}
+
+/*
+const isNotBorderPoint = (point: Point, polygon: Point[]):boolean => {
+    const isEdge = (point.lat == areaLimits.min.lat || point.long == areaLimits.min.long || 
+        point.lat == areaLimits.max.lat || point.long == areaLimits.max.long)  
+
+    return !isEdge && point.alt === polygon[Number.parseFloat((point.lat + pointAccuracy).toFixed(2))]?.[point.long]?.alt &&
+    point.alt === polygon[point.lat]?.[Number.parseFloat((point.long + pointAccuracy).toFixed(2))]?.alt &&
+    point.alt === polygon[Number.parseFloat((point.lat - pointAccuracy).toFixed(2))]?.[point.long]?.alt &&
+    point.alt === polygon[point.lat]?.[Number.parseFloat((point.long - pointAccuracy).toFixed(2))]?.alt
+}
+*/
