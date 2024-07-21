e19201566da6ceb9b3f3770631d5c25cb9d2cd29
yonatanbeery <yonatanbeery@gmail.com>
2024-07-21 22:54:13 +0300

    removed example point

--- coordinates/src/logic/convexHull.ts
+++ coordinates/src/logic/convexHull.ts
@@ -46,9 +46,3 @@ export const convexHull = (points:Point[]):Point[] => {
 
     return hull;
 }
-
-// Example usage
-const points = [
-    [0, 3], [1, 1], [2, 2], [4, 4],
-    [0, 0], [1, 2], [3, 1], [3, 3]
-];
